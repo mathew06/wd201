0a06ab66c262e3789f928fc3535f2089ebd3afd7
mathew06 <mathewabraham6603@gmail.com>
2025-05-09 12:00:11 +0000

    Adding files for http-server

---
+++ http-server/index.js
@@ -0,0 +1,52 @@
+const http = require("http");
+const fs = require("fs");
+const minimist = require("minimist");
+const args = minimist(process.argv.slice(2));
+
+const port = parseInt(args.port);
+
+let homeContent = "";
+let projectContent = "";
+let registrationContent = "";
+
+fs.readFile("home.html", (err, home) => {
+  if (err) {
+    throw err;
+  }
+  homeContent = home;
+});
+
+fs.readFile("project.html", (err, project) => {
+  if (err) {
+    throw err;
+  }
+  projectContent = project;
+});
+
+fs.readFile("registration.html", (err, registration) => {
+  if (err) {
+    throw err;
+  }
+  registrationContent = registration;
+});
+
+http
+  .createServer((req, res) => {
+    let url = req.url;
+    res.writeHeader(200, { "Content-Type": "text/html" });
+    switch (url) {
+      case "/project":
+        res.write(projectContent);
+        res.end();
+        break;
+      case "/registration":
+        res.write(registrationContent);
+        res.end();
+        break;
+      default:
+        res.write(homeContent);
+        res.end();
+        break;
+    }
+  })
+  .listen(port);
