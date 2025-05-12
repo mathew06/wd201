2c76a5f19f982c05a1a46d24e6e8ef7cc90f9e72
mathew06 <mathewabraham6603@gmail.com>
2025-05-12 12:09:39 +0000

    add database migrations and test environments

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,65 +1,19 @@
 /* eslint-disable no-undef */
-const todoList = require("../todo");
+const db = require("../models");
 
-const { all, add, markAsComplete } = todoList();
+describe("Todolist Test Suite", () => {
+  beforeAll(async () => {
+    await db.sequelize.sync({ force: true });
+  });
 
-describe("TodoList Test Suite", () => {
-  beforeAll(() => {
-    add({
-      title: "Test Todo",
+  test("Should add new todo", async () => {
+    const todoItemsCount = await db.Todo.count();
+    await db.Todo.addTask({
+      title: "Test todo",
       completed: false,
-      dueDate: new Date().toISOString().slice(0, 10),
+      dueDate: new Date(),
     });
-    add({
-      title: "Submit assignment",
-      dueDate: new Date(new Date().setDate(new Date().getDate() - 1))
-        .toISOString()
-        .slice(0, 10),
-      completed: false,
-    });
-    add({
-      title: "File taxes",
-      dueDate: new Date(new Date().setDate(new Date().getDate() + 1))
-        .toISOString()
-        .slice(0, 10),
-      completed: false,
-    });
-  });
-
-  test("Should add new todo", () => {
-    let todoCount = all.length;
-    add({
-      title: "Test Todo",
-      completed: false,
-      dueDate: new Date().toISOString().slice(0, 10),
-    });
-    expect(all.length).toBe(todoCount + 1);
-  });
-
-  test("Should mark todo as complete", () => {
-    expect(all[0].completed).toBe(false);
-    markAsComplete(0);
-    expect(all[0].completed).toBe(true);
-  });
-
-  test("Should return overdue todos", () => {
-    const overdueTodos = all.filter(
-      (todo) => todo.dueDate < new Date().toISOString().slice(0, 10),
-    );
-    expect(overdueTodos.length).toBe(1);
-  });
-
-  test("Should return todos due today", () => {
-    const todayTodos = all.filter(
-      (todo) => todo.dueDate === new Date().toISOString().slice(0, 10),
-    );
-    expect(todayTodos.length).toBe(2);
-  });
-
-  test("Should return todos due later", () => {
-    const laterTodos = all.filter(
-      (todo) => todo.dueDate > new Date().toISOString().slice(0, 10),
-    );
-    expect(laterTodos.length).toBe(1);
+    const newTodoItemsCount = await db.Todo.count();
+    expect(newTodoItemsCount).toBe(todoItemsCount + 1);
   });
 });
